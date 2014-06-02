122a8ca705f5757bc1344c1762eb3ceb01ea0b88
B.B. Shortcut <bbshortcut@alysse.org>
2014-06-02 21:16:32 +0200

    itemPred "safety" properly tested

--- test/tests.hs
+++ test/tests.hs
@@ -1,13 +1,15 @@
 module Main where
 
+import Data.List (nub)
 import Data.Monoid (mempty)
 import Test.Framework (defaultMainWithOpts)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
-import Test.QuickCheck ((==>), Property, quickCheckAll)
+import Test.QuickCheck ((==>), Property)
 import Utils (itemPred)
 
 prop_itemPred_safety :: Eq a => [a] -> a -> Property
 prop_itemPred_safety ns x =
+    nub ns == ns ==>
     not (null ns) ==>
     head ns == x ==>
       ns `itemPred` x == Nothing
