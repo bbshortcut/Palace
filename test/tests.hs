121677ac77169435cfe4c50135c61fed0fee62f6
B.B. Shortcut <bbshortcut@alysse.org>
2014-06-08 17:26:56 +0200

    test-framework replaced by tasty

--- test/tests.hs
+++ test/tests.hs
@@ -1,9 +1,9 @@
 module Main where
 
 import Data.List (nub)
-import Test.Framework (defaultMain)
-import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck ((==>), Property)
+import Test.Tasty (defaultMain)
+import Test.Tasty.QuickCheck (testProperty)
 import Utils (itemPred)
 
 prop_itemPred_bounded :: Eq a => [a] -> a -> Property
@@ -14,7 +14,6 @@ prop_itemPred_bounded ns x =
       ns `itemPred` x == Nothing
 
 main :: IO ()
-main = defaultMain
-       [ testProperty "itemPred bounded"
-                      (prop_itemPred_bounded :: [Int] -> Int -> Property)
-       ]
+main = defaultMain $
+       testProperty "itemPred bounded"
+                        (prop_itemPred_bounded :: [Int] -> Int -> Property)
